1207dd6e3fbc205c71c87a4d3b9098d44d181fc4
pedro <pedrolelis@hotmail.com>
2023-05-17 13:08:23 -0300

    aula 1

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        var enderecoDoArquivo = "contas.txt";
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open, FileAccess.Read);
+        var buffer = new byte[1024];
+        fluxoDoArquivo.Read(buffer, 0, 1024);
         Console.ReadLine();
     }
 }
